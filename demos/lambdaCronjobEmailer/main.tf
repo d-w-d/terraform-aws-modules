f1884215bea08f5cefceb55fb09231f98bbddb9e
d-w-d <ddarg@umd.edu>
2021-05-06 21:07:20 -0400

    Clean up cronjob demo

--- demos/lambdaCronjobEmailer/main.tf
+++ demos/lambdaCronjobEmailer/main.tf
@@ -1,11 +1,9 @@
-# Specify region
 provider "aws" {
   region = "us-east-1"
 }
 
-# Lambda-emailer-function module with api-gateway-integration module
 module "lambda_emailer_function_module" {
-  # source      = "git@d-w-d.github.com:d-w-d/terraform-aws-modules.git//modules/lambda_emailer_function"
+  # source                  = "git@d-w-d.github.com:d-w-d/terraform-aws-modules.git//modules/lambda_emailer_function"
   source                    = "../../modules/lambda_emailer_function"
   prefix                    = var.PREFIX
   lambda_function_name      = var.LAMBDA_FUNCTION_NAME
@@ -17,7 +15,7 @@ module "lambda_emailer_function_module" {
 }
 
 module "cloudwatch_triggering_lambda_module" {
-  # source      = "git@d-w-d.github.com:d-w-d/terraform-aws-modules.git//modules/cloudwatch_triggering_lambda"
+  # source             = "git@d-w-d.github.com:d-w-d/terraform-aws-modules.git//modules/cloudwatch_triggering_lambda"
   source               = "../../modules/cloudwatch_triggering_lambda"
   lambda_function_name = module.lambda_emailer_function_module.lambda_function_name
   lambda_function_arn  = module.lambda_emailer_function_module.lambda_function_arn
